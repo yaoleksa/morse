9719905ab23edddcc7ca788a03bac2b1b04d7295
yaoleksa <mryaremchyk@gmail.com>
2025-10-27 19:26:01 +0200

    added password generator page

---
+++ src/password-generator.js
@@ -0,0 +1,12 @@
+const pwdGen = (desiredLength) => {
+    let alphabet = document.getElementsByName("spec-char")[0].checked ? "0123456789" : "!#$%&*-_=+0123456789";
+    for(let i = 65; i < 65 + 26; ++i) alphabet += String.fromCharCode(i);
+    for(let i = 97; i < 97 + 26; ++i) alphabet += String.fromCharCode(i);
+    const alphabetLength = alphabet.length;
+    return Array.from({length: desiredLength}, v => alphabet[parseInt(Math.random() * alphabetLength)]).join("");
+}
+
+document.getElementsByClassName("submit-button")[0].addEventListener("click", () => {
+    // assign generated value as output text
+    document.getElementById("output").innerText = pwdGen(document.getElementById("desired-length").value); 
+});
